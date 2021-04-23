976bb4f926c06172fe2e8e5c6e15b21d617a1ffd
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2021-04-23 10:37:26 -0500

    added components

--- health-monitor-ui/src/app/app.module.ts
+++ health-monitor-ui/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidenavComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
